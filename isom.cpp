83abc0cd21f160bd678e054fa04c8b748cf30476
dsimon21 <58481402+dsimon21@users.noreply.github.com>
2023-12-06 08:33:21 -0500

    Add files via upload

--- isom.cpp
+++ isom.cpp
@@ -72,8 +72,8 @@ int main(int argc, char** argv) {
 
   parse_arguments(argc, argv);
   double adbc = a*d - b*c;
-  if (adbc < .000001 && adbc > -0.000001) {
-    printf("ad - bc cannot equal 0. Please try again.\n");
+  if (!(adbc < 1.000001 && adbc > .99999)) {
+    printf("ad - bc = %f. ad-bc must equal 1. Please try again.\n", adbc);
     exit(1);
   }
   print_commands();
